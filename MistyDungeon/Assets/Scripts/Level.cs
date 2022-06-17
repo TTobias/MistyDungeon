d1ef3c1965c1aae4f4c3bde6fd56f70c4e0489ed
TTobias <58746166+TTobias@users.noreply.github.com>
2022-06-18 00:02:40 +0200

    added stuff

--- MistyDungeon/Assets/Scripts/Level.cs
+++ MistyDungeon/Assets/Scripts/Level.cs
@@ -7,6 +7,7 @@ public class Level : MonoBehaviour
     [Header("Data")]
     public TileObject[,] map;
     public GameObject[,] mapObject;
+    public GameObject player;
     //public int enemies;
 
     [Header("Generator")]
@@ -39,6 +40,13 @@ public class Level : MonoBehaviour
         for(int x = 0; x<size; x++){
             for(int y = 0; y<size; y++){
                 map[x,y] = new TileObject();
+                /*
+                if(y<size/4 +1 && x+y<=size/4){ //bottom left corner
+                    map[x,y].isVoid = true;
+                }
+                if(x>(size/4)*3 && x+y>=(size/4)*3*2){ //top right corner
+                    map[x,y].isVoid = true;
+                }*/
             }
         }
         //set map Tiles
@@ -47,7 +55,8 @@ public class Level : MonoBehaviour
             for(int y = 0; y<size; y++){
                 mapObject[x,y] = Instantiate<GameObject>(tilePrefab);
                 mapObject[x,y].transform.position = new Vector2(x*tileWidth + tileWidth/2, y*tileHeight + x*0.5f*tileHeight + tileHeight/2);
-
+                //mapObject[x,y].GetComponent<SpriteRenderer>().enabled = !map[x,y].isVoid;
+                mapObject[x,y].GetComponent<SpriteRenderer>().color = map[x,y].isVoid?Color.black:Color.white;
             }
         }
 
